d6a6c8a0d3696961de64cd35baa44427d6a5ff5d
PeetGaming <110664522+JacobIkke@users.noreply.github.com>
2023-10-30 17:59:17 +0100

    Update Digital_Filtering_OverSampling.ino

--- Digital_Filtering_OverSampling.ino
+++ Digital_Filtering_OverSampling.ino
@@ -1,8 +1,8 @@
 // This code increases the ADC value, improves the accuracy of the ADC, and applies average filtering.
 // Only used 4 bytes of RAM as a buffer
 
-#define OVERSAMPLING_RATIO 500 // This increase the accuracy and average filtering
-#define RESOLUTION_INCREASE 10 // This increase the ADC value, in this case from 0-1024 to 0-10240
+#define OVERSAMPLING_RATIO 500 // This increases the accuracy and average filtering
+#define RESOLUTION_INCREASE 10 // This increases the ADC value, in this case from 0-1024 to 0-10240
 
 void setup() {
   Serial.begin(9600);
@@ -10,7 +10,6 @@ void setup() {
 
 void loop() {
 
-  while (1) {
     
         // Buffer to store the sampling values
         // uint32_t can hold a max of 2147483647, and 500 x 1024 = 512000, so that fits within a 32-bit integer.
@@ -28,6 +27,5 @@ void loop() {
         // Do something with the output value (e.g.,serial out, output to LEDs, etc.)
         Serial.println(averagedValue);
 
-        delay(100); 
-  }       
+        delay(100);   
 }
